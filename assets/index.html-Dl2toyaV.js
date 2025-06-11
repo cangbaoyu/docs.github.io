2ecf5a084b4d293c5e5e83c7165f900376cb887d
cangbaoyu <46984377+cangbaoyu@users.noreply.github.com>
2025-06-11 09:55:22 +0000

    Deploying to main from @ cangbaoyu/docs@4631e0b339691e78fb60e41f06117fdeadda22c9 🚀

---
+++ assets/index.html-Dl2toyaV.js
@@ -0,0 +1,19 @@
+import{_ as t}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as o,a as n,b as a,n as r,g as c,r as i,o as m}from"./app-CSRNZOmr.js";const p={};function s(d,l){const e=i("VPBanner");return m(),o("div",null,[n(e,r(c({title:"即时案例",content:"StackBlitz 驱动的在线模板",logo:"/广告.png",background:"var(--bg-2)",color:"var(--banner-text)",actions:[{text:"前往体验",link:"https://stackblitz.com/fork/vuepress-theme-hope"}]})),null,16),a(` ## 开源项目
+
+- [查看详情](./projects.md)
+
+## 即时案例页面
+
+- [使用功能展示的项目主页](./project-home.md)
+
+- [博客主页](./blog-home.md)
+
+- [自定义博客主页](./custom-blog-home.md)
+
+- [档案主页](./portfolio-home.md)
+
+- [加密文章](./encrypt.md) (密码: 1234)
+
+- <ProjectLink name="md-enhance" path="/zh/guide/content/revealjs/demo.html">幻灯片页面</ProjectLink>
+
+- [插槽演示](./slot.md) `)])}const u=t(p,[["render",s]]),_=JSON.parse('{"path":"/audition/","title":"宝典介绍","lang":"zh-CN","frontmatter":{"title":"宝典介绍","icon":"bars","index":false,"dir":{"order":1},"category":["介绍"],"tag":["介绍"],"head":[["meta",{"property":"og:url","content":"https://www.sansei.top/audition/"}],["meta",{"property":"og:site_name","content":"川上富江"}],["meta",{"property":"og:title","content":"宝典介绍"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2025-06-11T09:54:05.000Z"}],["meta",{"property":"article:tag","content":"介绍"}],["meta",{"property":"article:modified_time","content":"2025-06-11T09:54:05.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"宝典介绍\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2025-06-11T09:54:05.000Z\\",\\"author\\":[]}"]]},"headers":[],"git":{"createdTime":1749635645000,"updatedTime":1749635645000,"contributors":[{"name":"cangbao","email":"yph0228@163.com","commits":1}]},"readingTime":{"minutes":0.45,"words":135},"filePathRelative":"audition/README.md","localizedDate":"2025年6月11日"}');export{u as comp,_ as data};
